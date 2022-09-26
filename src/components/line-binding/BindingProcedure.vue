95a85fb4b90304cb549ea2bf98459357b89033ce
RWeng <rweng@ehanlin.com.tw>
2022-09-26 14:36:10 +0800

    這次加上 當綁定手機號碼時，該手機號碼同時有多位學生帳號，跳出select選單供user 選取的相關feature

--- src/components/line-binding/BindingProcedure.vue
+++ src/components/line-binding/BindingProcedure.vue
@@ -10,8 +10,20 @@
                         @given-mobile="givenMobile"></LineBindingInput>
     </p>
 
+    <mu-select v-if="isQueryMultipleStudent"
+               @change="checkSelected"
+               v-model="selected"
+               :error-text="errorText">
+
+      <mu-option disabled value="" label="請選擇帳號"></mu-option>
+      <mu-option v-for="student in students"
+                 :key="student.studentCard"
+                 :value="student.studentCard"
+                 :label="student.email"></mu-option>
+    </mu-select>
+
     <div class="button-div">
-      <mu-button @click="inputToPrevious" color="lightBlue" round class="color-primary" v-if="!isAlreadyBinding">上一步</mu-button>
+      <mu-button @click="goToPreviousStep" color="lightBlue" round class="color-primary" v-if="!isAlreadyBinding">上一步</mu-button>
       <mu-button @click="nextStep" color="lightBlue900" round v-if="isShowNextToConfirmBtn">下一步</mu-button>
     </div>
   </div>
@@ -31,7 +43,11 @@ export default {
 
   data() {
     return {
-      isShowNextToConfirmBtn: false
+      isShowNextToConfirmBtn: false,
+      isQueryMultipleStudent: false,
+      students: [],
+      selected: '',
+      errorText: ''
     }
   },
   methods: {
@@ -39,6 +55,8 @@ export default {
       this.isShowNextToConfirmBtn = !!studentCard;
       this.student.mobile = ''
       this.student.studentCard = studentCard
+      // 先觸發手機查詢到多位學生後，又切回選擇學號，需把該select 移除
+      this.isQueryMultipleStudent = false
     },
 
     givenMobile(mobile) {
@@ -47,7 +65,7 @@ export default {
       this.student.mobile = mobile
     },
 
-    inputToPrevious() {
+    goToPreviousStep() {
       this.isShowNextToInputBtn = false
       this.handlePrevious()
       this.student.role = ''
@@ -56,7 +74,45 @@ export default {
     },
 
     nextStep() {
-      this.handleNext()
+      this.checkStudentsCountWithMobile()
+
+      // 有選擇學號後 進行下一步，例如手機查詢到多位學生，確保select選單有選擇帳號
+      if (this.student.studentCard) {
+        this.handleNext()
+      }
+    },
+
+    checkStudentsCountWithMobile() {
+      // 還沒選擇學號 且 有輸入手機號碼，用手機號碼查詢學生是否有多位的情況
+      if (!this.student.studentCard && this.student.mobile) {
+        this.$axios(
+            {
+              method: 'get',
+              // url: `https://www.tbbt.com.tw/linebot/lineBinding/user?mobile=${this.student.mobile}`,
+              url: `/linebot/lineBinding/students-by-mobile?mobile=${this.student.mobile}`,
+            }
+        ).then(
+            response => {
+              this.students = response.data.content
+              if (this.students.length > 1) {
+                // 跳出select 選單
+                this.isQueryMultipleStudent = true
+                // reset select選單為 default
+                this.selected = ''
+                this.errorText = '請選擇欲綁定的一位學生帳號'
+              }
+            }
+        ).catch(error => {
+          console.error(error)
+        })
+      }
+    },
+
+    checkSelected() {
+      // 選擇學號，只有一開始會有default 未選擇的情況，因此才不用if else
+      // select只要觸發change 代表有選到學號，即可將errorText 清空
+      this.student.studentCard = this.selected
+      this.errorText = ''
     },
 
     ...mapActions('step', {
