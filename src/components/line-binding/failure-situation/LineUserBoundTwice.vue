35de8e8564c0c670bc2c8fc8bacb75b74a07e4b1
RWeng <rweng@ehanlin.com.tw>
2022-09-26 14:34:46 +0800

    目前先將 舊有的code上 綁定失敗的原因，拆分成各個component放置在該directory下

---
+++ src/components/line-binding/failure-situation/LineUserBoundTwice.vue
@@ -0,0 +1,22 @@
+<template>
+  <mu-col span="12">
+    <span class="verify-result font-important-info">您已綁定過此帳號囉，請至帳號查詢頁面查看詳情！</span>
+    <mu-button @click="queryProfiles" color="orange" round>帳號查詢</mu-button>
+  </mu-col>
+</template>
+
+<script>
+export default {
+  name: "LineUserBoundTwice",
+  props: ['lineUserId'],
+  methods: {
+    queryProfiles() {
+      this.$router.push(`/profile/${this.lineUserId}`)
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
