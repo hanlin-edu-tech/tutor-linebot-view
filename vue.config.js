f460246a3ae73acc4eed1c3418a240d249c8ce39
RWeng <rweng@ehanlin.com.tw>
2022-09-26 13:48:32 +0800

    新增設定，只有在dev mode時，會建立一個proxy 解決 前端send request 造成的cors問題

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,20 @@
 module.exports = {
   publicPath: './',
-  assetsDir: 'static'
+  assetsDir: 'static',
+  devServer: {
+    proxy: {
+      '/linebot': {
+        target: 'https://www.tbbt.com.tw/linebot/',
+        pathRewrite: { '^/linebot': '' },
+        changeOrigin: true,
+        ws: true
+      },
+      '/shop': {
+        target: 'https://www.tbbt.com.tw/shop/',
+        pathRewrite: { '^/shop': '' },
+        changeOrigin: true,
+        ws: true
+      }
+    }
+  }
 }
