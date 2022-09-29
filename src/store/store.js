7bd2b5cc5f25465ef0cd265ac88bfcf4773f7ef2
RWeng <rweng@ehanlin.com.tw>
2022-09-29 16:21:28 +0800

    增加帳號管理用的profile module

--- src/store/store.js
+++ src/store/store.js
@@ -3,13 +3,15 @@ import Vuex from 'vuex'
 import binding from './modules/binding'
 import step from './modules/step'
 import unifyDesc from './modules/unify-desc'
+import profile from "@/store/modules/profile";
 
 Vue.use(Vuex)
 const store = new Vuex.Store({
   modules: {
     binding,
     step,
-    unifyDesc
+    unifyDesc,
+    profile
   }
 })
 
